518871780a993398af288cf968c7bf518626a62b
Hossein Tajfirouz <hosseintajfirouz@gmail.com>
2023-01-23 17:55:22 +0400

    updated

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 
-
+# Import libraries
 import streamlit as st
 import pandas as pd
 import requests
 import datetime
 import plotly.figure_factory as ff
+import random
 # Change page names on Side bar
 st.set_page_config(
-    page_title="Task Tracker",
+    page_title="Task Logger",
 )
+
+
+# Css
+
+style = """
+<style>
+@import url("https://fonts.googleapis.com/css?family=Sofia");
+
+
+button[title="View fullscreen"]{
+    visibility: hidden;}
+
+
+.css-1ec096l {
+    font-size: 12px;
+    font-family: "Source Sans Pro", sans-serif;
+    padding: 0.25rem 0.375rem;
+    line-height: 1.5;
+    overflow: overlay;
+    text-align: center;
+}
+</style>
+"""
+
+#  css part
+st.write(style, unsafe_allow_html=True)
+
+
 
 
 d = st.date_input(
@@ -21,6 +50,7 @@ t2 = st.time_input('End Time', datetime.time(t.hour + 1 if t.hour < 23 else t.ho
 st.write('Date:', d, 'Start Time', t)
 
 st.sidebar.title("HT Task Tracker")
+
 EMAIL= st.sidebar.text_input("Enter Your Email Address")
 JIRA_TOKEN = st.sidebar.text_input("Enter Your Jira Token", type="password")
 
@@ -32,14 +62,10 @@ def get_issues(email:str, jira_token:str, d:object,t:object,t2:object):
     "Content-Type": "application/json"
     }
     data = []
-    for startIndex in range(0, 5, 1):
-        url = f"http://betconstruct.atlassian.net/rest/api/2/search?jql=created%20%3E=%20%22{d.year}/{d.month}/{d.day} {t.hour}:{t.minute} %22%20and%20created%20%3C=%20%22{d.year}/{d.month}/{d.day + 1} %22&project=VHD&fields=key,parent,summary,assignee,status,timeestimate,created,updated,&maxResults=100&startAt={startIndex}"
-
-  
+    for startIndex in range(0, 500, 100):
+        url = f"http://betconstruct.atlassian.net/rest/api/2/search?jql=project=VHD AND created%20%3E=%20%22{d.year}/{d.month}/{d.day} {t.hour}:{t.minute} %22%20and%20created%20%3C=%20%22{d.year}/{d.month}/{d.day + 1} %22&fields=key,parent,summary,assignee,status,timeestimate,created,updated,customfield_12513,customfield_12512,customfield_12511,reporter,priority,&maxResults=100&startAt={startIndex}"
         response = requests.get(url, headers=headers,
                                 auth=(email, jira_token))
-
-        
         try:
             resp = response.json()
             data.extend(resp['issues'])
@@ -50,6 +76,8 @@ def get_issues(email:str, jira_token:str, d:object,t:object,t2:object):
             break
 
     return filter_by_timestamp(data,t2)
+
+
 
 
 
@@ -67,7 +95,11 @@ def filter_by_timestamp(data: list, time):
     return filtered_issues
 
 
-
+def generate_random_hex_color():
+    random_number = random.randint(0,16777215)
+    hex_number = str(hex(random_number))
+    hex_number ='#'+ hex_number[2:]
+    return hex_number
 
 
 if not t >= t2:
@@ -81,18 +113,40 @@ if not t >= t2:
 
         else:
             real_data = []
-            colors_dict = {'Cancelled':'#F11212','Waiting for BetCo':'#Eb008a','Done':'#3DE785','Opened':'#3DE78D','Waiting for Reporter':'#FFFFFF','Prioritized':'#3D9CE7','Closed':'#DFCFBE', 'In Progress':'#55B4B0','Backlog':'#5B5EA6', 'Waiting for Review':'#EFC050','To Do':'#DEE7EC','Review':'#B565A7','On hold':'#E15D44','Waiting for Approval':'#3DE7DE'}
+            colors_dict = {'Ready for Development':'#FFFFF','Cancelled':'#F11212','Waiting for BetCo':'#FF00FF','Done':'#3DE785','Opened':'#3DE78D','Waiting for Reporter':'#FFFFFF','Prioritized':'#3D9CE7','Closed':'#DFCFBE', 'In Progress':'#55B4B0','Backlog':'#5B5EA6', 'Waiting for Review':'#EFC050','To Do':'#DEE7EC','Review':'#B565A7','On hold':'#E15D44','Waiting for Approval':'#3DE7DE'}
+           
             for issue in data:
                 issue_date = issue['fields']['created'].split('T')  # a list of two strings -> example: ['2023-01-23', '10:56:50.910+0400']
                 issue_update = issue['fields']['updated'].split('T')  # a list of two strings -> example: ['2023-01-23', '10:56:50.910+0400']
-                real_data.append(dict(Task=f"{issue['key']}", Start=f'{issue_date[0]} {issue_date[1][:8]}',Finish=f'{issue_update[0]} {issue_update[1][:8]}',Assignee =issue['fields']['assignee']['displayName'] ,Status=issue['fields']['status']['name'],Color=colors_dict.get(issue['fields']['status']['name'])))
-
+                #-({issue['fields']['status']['name']})
+                try:
+                    assignee = issue['fields']['assignee']['displayName']
+                except TypeError:
+                    assignee = 'Unknown'
+                try:
+                    reporter = issue['fields']['reporter']['displayName']
+                except TypeError:
+                    reporter = 'Unknown'
+                try:
+                    product_game = issue['fields']['customfield_12513']['value']
+                except TypeError:
+                    product_game = 'Unknown'
+                
+                try:
+                    impact_criticality = issue['fields']['customfield_12511']['value']
+                except TypeError:
+                    impact_criticality = 'Unknown'
+                try:
+                    skin = ",".join([sk['value'] for sk in issue['fields']['customfield_12512']])
+                except TypeError:
+                    skin = 'Unknown'
+                
+                real_data.append(dict(Task=f"{issue['key']}", Start=f'{issue_date[0]} {issue_date[1][:8]}',Finish=f'{issue_update[0]} {issue_update[1][:8]}',Assignee= assignee,Reporter=reporter,Status=issue['fields']['status']['name'],Impact_Criticality=impact_criticality,Product_Game=product_game,Skin=skin,Color=colors_dict.get(issue['fields']['status']['name'],generate_random_hex_color())))
+   
             df = pd.DataFrame(real_data)
-            c1,c2 = st.columns([0.5,0.5])
-            with c1:
-                st.table(df['Status'].value_counts())
-            with c2:
-                st.table(df['Assignee'].value_counts())
+            table_df = df.drop(['Color'], axis=1)
+
+            print('done')
             
             
             fig = ff.create_gantt(df,show_colorbar=True,index_col='Status',colors=list(map(lambda x: x, df['Color'])))
@@ -101,7 +155,25 @@ if not t >= t2:
                                 tickformat = '%d %B-%Y \n %H:%M:%S',
                             ))
 
+            # # Plot!
             st.plotly_chart(fig, use_container_width=True)
+            st.write("Tables")
+            st.table(table_df)
+            c1,c2,c3 = st.columns([0.5,0.5,0.5])
+            with c1:
+                st.table(df['Status'].value_counts())
+            with c2:
+                st.table(df['Assignee'].value_counts())
+            with c3:
+                st.table(df['Product_Game'].value_counts())
+            c4,c5,c6 = st.columns([0.7,0.5,0.5])
+            with c4:
+                st.table(df['Skin'].value_counts())
+            with c5:
+                st.table(df['Reporter'].value_counts())
+            with c6:
+                st.table(df['Impact_Criticality'].value_counts())
+            
 else:
     st.markdown("End Time is more than Start Time")
 
