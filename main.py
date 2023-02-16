f64e84539bfeb66965f2e3dc8df3456694fb0073
Hossein Tajfirouz <hosseintajfirouz@gmail.com>
2023-02-16 09:41:13 +0400

    advanced filter added and bugs fixed

--- main.py
+++ main.py
@@ -55,25 +55,27 @@ with st.expander("Advanced Search"):
         requested_product = st.selectbox("Product Game: ", ('All', 'Games', 'Payments', 'Casino',
                                          'Other', 'Sportsbook', 'Wonder Wheel', 'Skill Games', 'Games', 'Promotion', 'Live Casino'))
     with c7:
-        requested_status = st.selectbox("Status: ", ('All', 'Closed', 'Ready for Development', 'Cancelled', 'Waiting for BetCo', 'Done', 'Opened', 'Waiting for Reporter', 'Prioritized',
-                                        'In Progress', 'Backlog', 'Waiting for Review', 'To Do', 'Review', 'On hold', 'Waiting for Approval'))
-        # for index, cb in enumerate(('Closed', 'Ready for Development', 'Cancelled', 'Waiting for BetCo', 'Done', 'Opened', 'Waiting for Reporter', 'Prioritized',
-        #                             'Rejected', 'In Progress', 'Backlog', 'Waiting for Review', 'To Do', 'Review', 'On hold', 'Waiting for Approval')):
-        #     if index % 2 == 0:
-        #         with c9:
-        #             if index == 0:
-        #                 st.markdown("Choose Status")
-        #             check_box = st.checkbox(cb)
-        #     else:
-        #         with c10:
-        #             if index == 1:
-        #                 st.markdown("<br>", unsafe_allow_html=True)
-        #             check_box = st.checkbox(cb)
-        #     check_boxes[cb] = check_box
+        index_column = st.selectbox(
+            'Category By:',
+            ('Status', 'Product Game', 'Impact Criticality'))
+        # requested_status = st.selectbox("Status: ", ('All', 'Closed', 'Ready for Development', 'Cancelled', 'Waiting for BetCo', 'Done', 'Opened', 'Waiting for Reporter', 'Prioritized',
+        #                                 'In Progress', 'Backlog', 'Waiting for Review', 'To Do', 'Review', 'On hold', 'Waiting for Approval'))
 
-    index_column = st.selectbox(
-        'Category By:',
-        ('Status', 'Product Game', 'Skin', 'Impact Criticality'))
+    for index, cb in enumerate(('Closed', 'Ready for Development', 'Cancelled', 'Waiting for BetCo', 'Done', 'Opened', 'Waiting for Reporter', 'Prioritized',
+                                'Rejected', 'In Progress', 'Backlog', 'Waiting for Review', 'To Do', 'Review', 'On hold', 'Waiting for Approval')):
+        if index % 2 == 0:
+            with c9:
+                if index == 0:
+                    st.markdown("Choose Status:")
+                check_box = st.checkbox(cb)
+        else:
+            with c10:
+                if index == 1:
+                    st.markdown("<br>", unsafe_allow_html=True)
+                check_box = st.checkbox(cb)
+        check_boxes[cb] = check_box
+
+
 st.write('Date:', d, 'Start Time', t)
 
 st.sidebar.title("HT Task Tracker")
@@ -113,19 +115,20 @@ def get_issues(email: str, jira_token: str, d: object, t: object, t2: object):
 def filter_by_timestamp(data: list, time):
     filtered_issues = []
     for issue in data:
-        # try:
-        #     if not check_boxes['All']:
-        #         if not check_boxes[issue['fields']['status']['name']]:
-        #             continue
-        # except KeyError:
-        #     continue
-        if requested_status != 'All':
-            if issue['fields']['status']['name'] != requested_status:
-                continue
+        try:
+            if not check_boxes['All']:
+                if not check_boxes[issue['fields']['status']['name']]:
+                    continue
+        except KeyError:
+            continue
+        # if requested_status != 'All':
+        #     if issue['fields']['status']['name'] != requested_status:
+        #         continue
 
         # if requested_product != 'All':
         #     if issue['fields']['customfield_12513']['value'] != requested_product:
         #         continue
+
         # a list of two strings -> example: ['2023-01-23', '10:56:50.910+0400']
         issue_date_and_time = issue['fields']['created'].split('T')
         # string format of the time - example -> 10:29:36
@@ -156,8 +159,8 @@ if not t >= t2:
 
         else:
             real_data = []
-            colors_dict = {'Ready for Development': '#FFFFF', 'Cancelled': '#F11212', 'Waiting for BetCo': '#072954', 'Done': '#3DE785', 'Opened': '#3DE78D', 'Waiting for Reporter': '#3182ed', 'Prioritized': '#3D9CE7',
-                           'Closed': '#DFCFBE', 'In Progress': '#55B4B0', 'Backlog': '#5B5EA6', 'Waiting for Review': '#EFC050', 'To Do': '#DEE7EC', 'Review': '#B565A7', 'On hold': '#E15D44', 'Waiting for Approval': '#3DE7DE'}
+            # colors_dict = {'Ready for Development': '#FFFFF', 'Cancelled': '#F11212', 'Waiting for BetCo': '#072954', 'Done': '#3DE785', 'Opened': '#3DE78D', 'Waiting for Reporter': '#3182ed', 'Prioritized': '#3D9CE7',
+            #                'Closed': '#DFCFBE', 'In Progress': '#55B4B0', 'Backlog': '#5B5EA6', 'Waiting for Review': '#EFC050', 'To Do': '#DEE7EC', 'Review': '#B565A7', 'On hold': '#E15D44', 'Waiting for Approval': '#3DE7DE'}
 
             for issue in data:
                 # a list of two strings -> example: ['2023-01-23', '10:56:50.910+0400']
@@ -188,23 +191,33 @@ if not t >= t2:
                 except TypeError:
                     skin = 'Unknown'
 
+                # real_data.append(dict(Task=f"{issue['key']}", Start=f'{issue_date[0]} {issue_date[1][:8]}', Finish=f'{issue_update[0]} {issue_update[1][:8]}', Assignee=assignee, Reporter=reporter,
+                #                       Status=issue['fields']['status']['name'], Impact_Criticality=impact_criticality, Product_Game=product_game, Skin=skin, Color=colors_dict.get(issue['fields']['status']['name'], generate_random_hex_color())))
+
                 real_data.append(dict(Task=f"{issue['key']}", Start=f'{issue_date[0]} {issue_date[1][:8]}', Finish=f'{issue_update[0]} {issue_update[1][:8]}', Assignee=assignee, Reporter=reporter,
-                                      Status=issue['fields']['status']['name'], Impact_Criticality=impact_criticality, Product_Game=product_game, Skin=skin, Color=colors_dict.get(issue['fields']['status']['name'], generate_random_hex_color())))
+                                      Status=issue['fields']['status']['name'], Impact_Criticality=impact_criticality, Product_Game=product_game, Skin=skin, Color=generate_random_hex_color()))
 
             df = pd.DataFrame(real_data)
-            table_df = df.drop(['Color'], axis=1)
+            # table_df = df.drop(['Color'], axis=1)
 
-            fig = ff.create_gantt(df, show_colorbar=True, index_col=index_column.replace(
-                ' ', '_'), colors=list(map(lambda x: x, df['Color'])))
+            if df.shape[0] > 36:
+                st.markdown('<b style="font-size:12px;color:#DD4124">Too many issues to show in the chart, we suggest to choose shorter time span or apply advanced filters</b>',unsafe_allow_html=True)
+
+            fig = ff.create_gantt(df, show_colorbar=True, colors=df['Color'],
+                                  index_col=index_column.replace(' ', '_'))
+
+            # to update legend ==> legend=dict(font=dict(size=8, color="red"))
             fig.update_layout(xaxis=dict(
                 title='Timestamp',
                 tickformat='%d %B-%Y \n %H:%M:%S',
-            ))
-
+            ),)
             # # Plot!
             st.plotly_chart(fig, use_container_width=True)
+
+            # Tables
             st.write("Tables")
-            st.table(table_df)
+            # st.table(table_df)
+            st.table(df)
             c1, c2, c3 = st.columns([0.5, 0.5, 0.5])
             with c1:
                 st.table(df['Status'].value_counts())
